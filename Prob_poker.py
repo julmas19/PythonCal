42df24ef3e574caaba65ee33b1a94e45bdaa14bd
julmas19 <61039738+julmas19@users.noreply.github.com>
2022-02-20 14:36:44 -0500

    Create Prob_poker.py

---
+++ Prob_poker.py
@@ -0,0 +1,28 @@
+lista1 = ('A','2','3','4','5','6','7','8','9','10','J','Q','K')
+lista2 = ("Corazones","Diamantes","Trebol","Picas")
+#Probabilidad de sacar 3 ASES
+baraja = list()
+repeticiones = 4000000
+mano = list()
+cont = 0
+AS3 = 0
+for k in range(repeticiones):
+  for i in lista1:
+    for j in lista2:
+      baraja.append(i+'-'+j)
+  for _ in range(5):
+    rango = len(baraja)-1
+    num = random.randint(0,rango)
+    valor = baraja[num]
+    baraja.pop(num)
+    cola.append(valor)
+    mano.append(valor)
+  for i in mano:
+    if(i.split('-')[0]=='A'):
+      cont = cont + 1
+    if(cont==3):
+      AS3 = AS3 + 1
+      cont=0
+  baraja.clear()
+  mano.clear()
+  cont=0 
